56b283f6efe472c1e86ad05a07ac081d85186c51
LEEYEYEONG <lee03876@catholic.ac.kr>
2025-04-19 10:54:59 +0900

    feat : stage2 배경 조정(수정필요)

--- src/components/Stage2.tsx
+++ src/components/Stage2.tsx
@@ -1,13 +1,13 @@
 import "./Stage2.css";
-import blackScreen from "../resources/images/black.png";
+import screenArea from "../resources/images/black.png";
 import hsBg from "../resources/images/hs/hbg.png";
 
 // Heart Star
 export function Stage2() {
   return (
     <div className="stage2-container">
-      <img src={blackScreen} alt="screenArea" className="blackScreen" />
-      {/* <img src={hsBg} alt="hsBg" className="hsBg" /> */}
+      <img src={screenArea} alt="screenArea" className="screenArea" />
+      <img src={hsBg} alt="hsBg" className="hsBg" />
     </div>
   );
 }

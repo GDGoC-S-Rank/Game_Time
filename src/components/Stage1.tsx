5adaa6f5c79d1059273d654401c4872e5f02bbc8
LEEYEYEONG <lee03876@catholic.ac.kr>
2025-05-05 22:12:42 +0900

    feat : 키보드 입력 시 효과 커스텀 훅 추가
    수정이 필요하다.
    아직 1p 눌렀을 떄만 효과가 나타나게 설정함(2p 도 추가해야 함)

--- src/components/Stage1.tsx
+++ src/components/Stage1.tsx
@@ -1,12 +1,27 @@
 import "./Stage1.css";
+import React, { useState, useEffect } from "react";
 import screenBorder from "../resources/images/screenBorder.png";
 import bgImage from "../resources/images/startScene/bg.png";
+import control_1p from "../resources/images/fontImg/minicube.png";
+import { useSpaceKeyEffect } from "../hooks/useSpaceKeyEffect";
 
 // Start Scene(Space is key)
 export function Stage1() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useSpaceKeyEffect(() => {
+    setIsVisible(true);
+    setTimeout(() => setIsVisible(false), 300);
+  });
+
   return (
     <div className="stage1-container">
       <img src={screenBorder} alt="screenBorder" className="screenBorder" />
+      <img
+        src={control_1p}
+        alt="contral_1p"
+        className={`control_1p ${isVisible ? "visible" : ""}`}
+      />
       <img src={bgImage} alt="bg" className="bgImage" />
     </div>
   );
--- src/components/Stage2.tsx
+++ src/components/Stage2.tsx
@@ -1,15 +1,38 @@
 import "./Stage2.css";
+import React, { useState, useEffect } from "react";
 import screenBorder from "../resources/images/screenBorder.png";
 import heartBg from "../resources/images/hs/hbg.png";
 import hfloor from "../resources/images/hs/hfloor.png";
+import control_panel from "../resources/images/fontImg/15.png";
+import control_1p from "../resources/images/fontImg/cube.png";
+import control_2p from "../resources/images/fontImg/exchangeRB.png";
+import { useSpaceKeyEffect } from "../hooks/useSpaceKeyEffect";
 
 // Heart Star
 export function Stage2() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useSpaceKeyEffect(() => {
+    setIsVisible(true);
+    setTimeout(() => setIsVisible(false), 300);
+  });
+
   return (
     <div className="stage2-container">
       <img src={screenBorder} alt="screenBorder" className="screenBorder" />
       <img src={hfloor} alt="hfloor" className="hfloor" />
       <img src={heartBg} alt="heartBg" className="heartBg" />
+      <img src={control_panel} alt="control_panel" className="control_panel" />
+      <img
+        src={control_1p}
+        alt="control_1p"
+        className={`control_1p ${isVisible ? "visible" : ""}`}
+      />
+      <img
+        src={control_2p}
+        alt="control_2p"
+        className={`control_2p ${isVisible ? "visible" : ""}`}
+      />
     </div>
   );
 }
